e28352b4b47c0ef43a1c88963de2bd8407655886
Administrator <kuailedetanger@163.com>
2025-12-03 11:00:28 +0800

    为什么几乎很少使用Timer这种方式？Timer底层是使用一个单线来实现多个Timer任务处理的，所有任务都是由同一个线程来调度，所有任务都是串行执行，

--- 420-springboot-demo-schedule-timer/src/main/java/tech/pdai/springboot/schedule/timer/timertest/TimerTester.java
+++ 420-springboot-demo-schedule-timer/src/main/java/tech/pdai/springboot/schedule/timer/timertest/TimerTester.java
@@ -8,6 +8,14 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 /**
+ *
+ * 为什么几乎很少使用Timer这种方式？Timer底层是使用一个单线来实现多个Timer任务处理的，所有任务都是由同一个线程来调度，所有任务都是串行执行，意味着同一时间只能有一个任务得到执行，而前一个任务的延迟或者异常会影响到之后的任务。如果有一个定时任务在运行时，产生未处理的异常，那么当前这个线程就会停止，那么所有的定时任务都会停止，受到影响。
+ * ------
+ * 著作权归@pdai所有
+ * 原文链接：https://pdai.tech/md/spring/springboot/springboot-x-task-timer.html
+ *
+ *
+ *
  * @author pdai
  */
 @Slf4j
